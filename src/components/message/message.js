2ec4a1b78754e39b6b40ab73303f5730ba9b9b5b
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-06-19 13:37:38 -0700

    refactor to modules

---
+++ src/components/message/message.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Message extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {};
+    this.state.text = 'Hello World';
+  }
+
+  render() {
+    return (
+      <div>{this.state.text}</div>
+    );
+  }
+}
+
